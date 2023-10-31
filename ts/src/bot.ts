70bce37bd76efd359070c340d4de4adf046b03d9
dood <74739472+itsamedood@users.noreply.github.com>
2023-10-31 10:35:23 -0400

    Started on command.ts
    Also the installed dependencies listed in package.json aren't written by me, but by npm when the install command is called.

--- ts/src/bot.ts
+++ ts/src/bot.ts
@@ -2,7 +2,7 @@ import { Client, ClientOptions, Collection } from "discord.js";
 
 export default class Bot extends Client {
   public commands = new Collection<string, Command>();
-  private commandJSON: CommandData[] = [];
+  private _commandJSON: CommandData[] = [];
 
   constructor(options: ClientOptions) {
     super(options);
--- ts/src/types/command.ts
+++ ts/src/types/command.ts
@@ -1,1 +1,10 @@
 import { ChatInputCommandInteraction } from "discord.js";
+import Bot from "../bot";
+
+export declare type Category = "MISC" | "MODERATION";
+
+export enum OptionType {
+  SUB_COMMAND = 1,
+  SUB_COMMAND_GROUP = 2,
+  STRING = 3,
+}
